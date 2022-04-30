4408d08687c4650daf352a5c5b2ce9d85eecb879
R0mb0 <f.rombaldoni@campus.uniurb.it>
2022-04-30 12:23:54 +0200

    Added More List Tools

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -5,6 +5,11 @@
 
 /***** List Tools Module *****/
 
+/** Tool to Get a List Element by Index **/
+get_by_Index(0, [X], X).
+get_by_Index(0, [H|_], H).
+get_by_Index(I, [_|T], E) :- NewIndex is I-1, get_by_Index(NewIndex, T, E).
+
 /** Tool to  Remove Head's Elements from an Index **/
 remove_head([_|Tail], Tail).
 
@@ -16,6 +21,7 @@ remove_from_head(N, Lst, Flst) :-
         remove_head(Lst, Remain),
         remove_from_head(N1, Remain, Flst)
     ).
+
 
 /** Tool to  Remove Tail's Elements from an Index **/
 remove_tail(X,Y):-
@@ -51,11 +57,29 @@ maintain_tail(N, Lst, Flst) :-
         maintain_tail(N, Remain, Flst)
     ).
 
+/** Tool to Maintain a Index of Elemets from the Head of List **/
+take_list(N, Lst, Flst):-
+    length(Lst, Len),
+    Num is Len - N,
+    remove_from_tail(Num, Lst, Flst).
+
+/** Tool to Maintain a Index of Elemets from the Tail of List **/
+drop_list(N, Lst, Flst):-
+    length(Lst, Len),
+    Num is Len - N,
+    remove_from_head(Num, Lst, Flst).
 /***** End *****/
 
 /***** Detection Module *****/
 split(Lst, Flst) :-
     remove_from_tail(17, Lst, Flst).
+
+getLatitude(Lst, Tpl) :-
+    get_by_Index(Lst, 0, Sign),
+    drop_list(Lst, 2, Rem),
+    take_list(Rem, 2, Degree),
+    Tpl = [0, 1].
+
 /***** End *****/
 
 /***** Main & Main Util *****/
@@ -77,9 +101,9 @@ main :-
     write('Insert a string...'), nl,
     read(A), nl,
     atom_chars(A, Str),
-    write('Insert index...'), nl,
-    read(Index), nl,
-    maintain_tail(Index, Str, Remain),
+    /*write('Insert index...'), nl,
+    read(Index), nl,*/
+    getLatitude(Str, Remain),
     write(Remain).
 
 
