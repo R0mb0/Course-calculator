1c427b3bbb1baa7e0c9ffd993cbfa3e35f72aa1e
R0mb0 <f.rombaldoni@campus.uniurb.it>
2022-05-21 11:10:09 +0200

    Minor Fixes

--- Report/Haskell_Tests/6-Check_Degrees.sh
+++ Report/Haskell_Tests/6-Check_Degrees.sh
@@ -1,5 +1,5 @@
 Detections Properties Calculator V1.0 
-Waring: The Detections must be in D.M.G format and inserted into the program like: N 40 45 36.000 - E 73 59 2.400
+Warning: The Detections must be in D.M.G format and inserted into the program like: N 40 45 36.000 - E 73 59 2.400
 Insert the First Detection...
 N 90 54 16.000 - E 12 54 30.000
 Insert the Second Detection...
