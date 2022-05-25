f02517edeca289948bd5443d429c54f704a91f94
Francesco Rombaldoni <72658034+R0mb0@users.noreply.github.com>
2022-05-25 14:51:07 +0200

    Minor Fixes

--- Report/Prolog_Tests/1-String_Formatting.sh
+++ Report/Prolog_Tests/1-String_Formatting.sh
@@ -1,5 +1,5 @@
 Detections Properties Calculator V1.0
-Warning: The Detections must be in D.M.G format and inserted into the program like: `N 40 45 36.000 - E 73 59 2.400`.
+Warning: The Detections must be in D.M.G format and inserted into the program like: `N 40 45 36.000 - E 073 59 2.400`.
 Insert the First Detection...
 `43 54 16.000  E 012 54 30.000`.
 Insert the Second Detection...
